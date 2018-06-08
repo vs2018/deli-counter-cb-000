28c712946fa0fc5f7af65c03ddb930ccd2c39610
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 14:34:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   katz_deli.each_with_index do |customer, index|
-    "Welcome, #{customer}. You are number #{index + 1} in line."
+    "Welcome, #{custovemer}. You are number #{index + 1} in line."
   end
   
 end
