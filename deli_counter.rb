443a8f9f0bf3c9d217f3f6a653770ca647ed7692
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 14:23:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,18 @@
 # Write your code here.
 
-def line(array)
-  puts "The line is currently empty." if array.size == 0
+def line(katz_deli)
+  puts "The line is currently empty." if katz_deli.size == 0
   
 end
 
-def take_a_number(array, name)
-  array << name
-  array.each_with_index do |customer, index|
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  katz_deli.each_with_index do |customer, index|
     "Welcome, #{customer}. You are number #{index + 1} in line."
   end
   
 end
 
-def now_serving(array)
+def now_serving(katz_deli)
   
 end
